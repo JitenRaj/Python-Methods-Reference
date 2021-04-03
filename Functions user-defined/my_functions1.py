937fbcbcb88ce5addacf25b245c3782ef154f09d
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-04-03 11:10:05 +0530

    .

---
+++ Functions user-defined/my_functions1.py
@@ -0,0 +1,12 @@
+"""User Defined functions are functions which  are created by users it-self
+for their requirements by using def-statement, when pre-defined functions
+are not enough / just don't cut it."""
+
+def self_intro():
+    print("I am Jitendra Singh Rajpurohit.")
+    print("I am pursuing my Engineering in Electronics and Communication Domain at School of Engineering & Technology, Jain University.")
+    print("I have learned to become obsessed with the problem - it allows me to derive the most valuable insights for potential solutions.")
+    print("I solve problems in creative ways. Recently started my journey as Web Developer and Web Designer.")
+
+self_intro()
+self_intro()
