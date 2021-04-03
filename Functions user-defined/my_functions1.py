f3ae44d277d066b0498305630e1c6e23b805ad22
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-04-03 11:11:06 +0530

    .

--- Functions user-defined/my_functions1.py
+++ Functions user-defined/my_functions1.py
@@ -2,11 +2,20 @@
 for their requirements by using def-statement, when pre-defined functions
 are not enough / just don't cut it."""
 
+# Here i have defined a function self_intro
+# self_intro contains 4 lines of introductions to print
+
 def self_intro():
     print("I am Jitendra Singh Rajpurohit.")
     print("I am pursuing my Engineering in Electronics and Communication Domain at School of Engineering & Technology, Jain University.")
     print("I have learned to become obsessed with the problem - it allows me to derive the most valuable insights for potential solutions.")
     print("I solve problems in creative ways. Recently started my journey as Web Developer and Web Designer.")
 
+
+# Here i have called self_intro function 2 times
+# It prints 4 lines of introduction each time we call it
+# We can call them multiple times in our code
+
 self_intro()
+print("\n")
 self_intro()
