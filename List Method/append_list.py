94c995a5f9b754c1193c1958e1a057975eef7eb3
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-23 23:14:22 +0530

    append list

--- List Method/append_list.py
+++ List Method/append_list.py
@@ -1,4 +1,16 @@
+#Add an element to the fruits list:
+
 fruits = ['apple','banana','cherry']
 print(fruits)
 fruits.append('orange')
 print(fruits)
+
+# Add a list to a list:
+
+a = ["apple", "banana", "cherry"]
+print(a)
+b = ["Ford", "BMW", "Volvo"]
+print(b)
+a.append(b)
+print(a)
+
