d17c5f320546506b692dacec499595e7c56eb25f
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-24 23:33:16 +0530

    Count String

---
+++ String Method/count_string.py
@@ -0,0 +1,9 @@
+# Return the number of times the value "Jiten" appers in the string
+
+txt = "Hello, I'm Jiten, and I'm in 3rd year."
+
+Iam = txt.count("I'm")
+
+Jiten = txt.count("Jiten")
+
+print("I'm appeared " + str(Iam) + " times and Jiten appeared " + str(Jiten) + " times")
