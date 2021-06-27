48f7caa5a6a83870a8377a108725a3d8c24539fc
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-27 23:34:29 +0530

    Is alphabetic String

---
+++ String Method/isalpha_string.py
@@ -0,0 +1,15 @@
+# Check if all the characters in the text are letters:
+
+txt = "CompanyX"
+
+x = txt.isalpha()
+
+print(x)
+
+# Check if all the characters in the text is alphabetic:
+
+txt = "Company10"
+
+x = txt.isalpha()
+
+print(x)
