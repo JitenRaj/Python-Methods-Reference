499b326d5d648ab41c6b65c4e2dbd3ffb877496b
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-27 23:32:56 +0530

    is Alpha-numeric String

---
+++ String Method/isalnum_string.py
@@ -0,0 +1,15 @@
+# Check if all the characters in the text are alphanumeric:
+
+txt = "Company12"
+
+x = txt.isalnum()
+
+print(x)
+
+# Check if all the characters in the text is alphanumeric:
+ 
+txt = "Company 12"
+
+x = txt.isalnum()
+
+print(x)
