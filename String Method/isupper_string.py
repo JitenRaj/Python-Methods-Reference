da60d72ac944e0dc4ddb4131b4dd5403831aa686
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-28 22:35:08 +0530

    is upper string

---
+++ String Method/isupper_string.py
@@ -0,0 +1,17 @@
+# Check if all the characters in the text are in upper case:
+
+txt = "THIS IS NOW!"
+
+x = txt.isupper()
+
+print(x)
+
+# Check if all the characters in the texts are in upper case:
+
+a = "Hello World!"
+b = "hello 123"
+c = "MY NAME IS PETER"
+
+print(a.isupper())
+print(b.isupper())
+print(c.isupper())
