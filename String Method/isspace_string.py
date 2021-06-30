7f141019b64c1d888174c866abf7666e052086cf
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-30 23:17:41 +0530

    is space in string

---
+++ String Method/isspace_string.py
@@ -0,0 +1,15 @@
+# Check if all the characters in the text are whitespaces:
+
+txt = "   "
+
+x = txt.isspace()
+
+print(x)
+
+# Check if all the characters in the text are whitespaces:
+
+txt = "   s   "
+
+x = txt.isspace()
+
+print(x)
