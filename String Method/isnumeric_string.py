5c8e1c26dbe0a2145f9ab300ae399ad424def363
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-30 23:12:58 +0530

    is numeric string

---
+++ String Method/isnumeric_string.py
@@ -0,0 +1,21 @@
+# Check if all the characters in the text are numeric:
+
+txt = "565543"
+
+x = txt.isnumeric()
+
+print(x)
+
+# Check if the characters are numeric:
+
+a = "\u0030" #unicode for 0
+b = "\u00B2" #unicode for &sup2;
+c = "10km2"
+d = "-1"
+e = "1.5"
+
+print(a.isnumeric())
+print(b.isnumeric())
+print(c.isnumeric())
+print(d.isnumeric())
+print(e.isnumeric())
