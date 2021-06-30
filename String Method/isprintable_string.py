9e08c8f850014f74d0ab0d813d6b9edadb0ae396
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-30 23:15:20 +0530

    is printable string

---
+++ String Method/isprintable_string.py
@@ -0,0 +1,15 @@
+# Check if all the characters in the text are printable:
+
+txt = "Hello! Are you #1?"
+
+x = txt.isprintable()
+
+print(x)
+
+# Check if all the characters in the text are printable:
+
+txt = "Hello!\nAre you #1?"
+
+x = txt.isprintable()
+
+print(x)
