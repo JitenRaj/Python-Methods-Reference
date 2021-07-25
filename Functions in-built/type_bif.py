144336da8f8fd2715d88397c7afe1a880956a39f
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-25 22:52:16 +0530

    type() function

---
+++ Functions in-built/type_bif.py
@@ -0,0 +1,15 @@
+# Return the type of these objects:
+
+a = ('apple', 'banana', 'cherry')
+b = "Hello World"
+c = 33
+d = 4.5
+e = 4.84939392959
+f = True
+
+x = type(a)
+y = type(b)
+z = type(c)
+p = type(d)
+q = type(e)
+r = type(f)
