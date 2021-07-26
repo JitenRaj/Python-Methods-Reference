5d84f4b7a5d1be0c897d038d6be09536299b8f77
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-26 17:53:46 +0530

    any() function

--- Functions in-built/any_bif.py
+++ Functions in-built/any_bif.py
@@ -2,21 +2,22 @@
 
 mylist = [False, True, False]
 x = any(mylist)
-
+print(x)
 
 # Check if any item in a tuple is True:
 
 mytuple = (0, 1, False)
 x = any(mytuple)
-
+print(x)
 
 # Check if any item in a set is True:
 
 myset = {0, 1, 0}
 x = any(myset)
-
+print(x)
 
 # Check if any item in a dictionary is True:
 
 mydict = {0 : "Apple", 1 : "Orange"}
 x = any(mydict)
+print(x)
