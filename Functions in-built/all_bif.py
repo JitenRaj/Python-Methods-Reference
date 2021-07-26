44bbbf77551f0abee001ef082105207b9f1ff674
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-26 17:53:12 +0530

    all() function

--- Functions in-built/all_bif.py
+++ Functions in-built/all_bif.py
@@ -2,23 +2,28 @@
 
 mylist = [True, True, True]
 x = all(mylist)
+print(x)
 
 # Check if all items in a list are True:
 
 mylist = [0, 1, 1]
 x = all(mylist)
+print(x)
 
 # Check if all items in a tuple are True:
 
 mytuple = (0, True, False)
 x = all(mytuple)
+print(x)
 
 # Check if all items in a set are True:
 
 myset = {0, 1, 0}
 x = all(myset)
+print(x)
 
 # Check if all items in a dictionary are True:
 
 mydict = {0 : "Apple", 1 : "Orange"}
 x = all(mydict)
+print(x)
