251e588994d7c9c8bf3a4168d6605c94aefb18e6
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-31 23:41:07 +0530

    input() function

---
+++ Functions in-built/input_bif.py
@@ -0,0 +1,10 @@
+# Ask for the user's name and print it:
+
+print('Enter your name:')
+x = input()
+print('Hello, ' + x)
+
+# Use the prompt parameter to write a message before the input:
+
+x = input('Enter your name:')
+print('Hello, ' + x)
