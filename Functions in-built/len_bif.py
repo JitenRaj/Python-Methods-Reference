b3e065e79b0c94235926df8e072a02841b3da6c4
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-31 23:53:35 +0530

    len() function

---
+++ Functions in-built/len_bif.py
@@ -0,0 +1,11 @@
+# Return the number of items in a list:
+
+mylist = ["apple", "banana", "cherry"]
+x = len(mylist)
+print(x)
+
+# Return the number of characters in a string:
+
+mystr = "Hello"
+y = len(mystr)
+print(y)
