d152885ff924f946c933c2abeca9fe9b8ba26374
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-01 00:01:10 +0530

    map() function

--- Functions in-built/map_bif.py
+++ Functions in-built/map_bif.py
@@ -10,3 +10,15 @@ print(type(x))
 
 #convert the map into a list, for readability:
 print(list(x))
+
+# Make new fruits by sending two iterable objects into the function:
+
+def myfunc(a, b):
+  return a + b
+
+x = map(myfunc, ('apple', 'banana', 'cherry'), ('orange', 'lemon', 'pineapple'))
+
+print(x)
+
+#convert the map into a list, for readability:
+print(list(x))
