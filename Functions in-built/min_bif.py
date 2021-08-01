fa79e810eb8ee5297ae9bacb0c7be7e06b6f69d4
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-01 23:33:41 +0530

    min() function

---
+++ Functions in-built/min_bif.py
@@ -0,0 +1,15 @@
+# Return the lowest number:
+
+x = min(5, 10)
+print(x)
+
+# Return the name with the lowest value, ordered alphabetically:
+
+x = min("Mike", "John", "Vicky")
+print(x)
+
+# Return the item in a tuple with the lowest value:
+
+a = (1, 5, 3, 9)
+x = min(a)
+print(x)
