ae98fe4db8a3fbc0a7a18ca78c8b179becc3fe07
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-01 23:35:20 +0530

    memoryview() function

---
+++ Functions in-built/memoryview_bif.py
@@ -0,0 +1,11 @@
+# Create and print a memoryview object:
+
+x = memoryview(b"Hello")
+
+print(x)
+
+#return the Unicode of the first character
+print(x[0])
+
+#return the Unicode of the second character
+print(x[1])
