131b3c84665af044ecb1df4d6adf79c245e8502c
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-05 23:09:32 +0530

    sum() function

---
+++ Functions in-built/sum_bif.py
@@ -0,0 +1,11 @@
+# Add all items in a tuple, and return the result:
+
+a = (1, 2, 3, 4, 5)
+x = sum(a)
+print(x)
+
+# Start with the number 7, and add all the items in a tuple to this number:
+
+a = (1, 2, 3, 4, 5)
+x = sum(a, 7)
+print(x)
