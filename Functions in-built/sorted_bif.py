6ddbf70f960f0556831191719bfd17eae5e4891b
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-05 23:06:10 +0530

    sorted() function

---
+++ Functions in-built/sorted_bif.py
@@ -0,0 +1,23 @@
+# Sort a tuple:
+
+a = ("b", "g", "a", "d", "f", "c", "h", "e")
+x = sorted(a)
+print(x)
+
+# Sort numeric:
+
+a = (1, 11, 2)
+x = sorted(a)
+print(x)
+
+# Sort ascending:
+
+a = ("h", "b", "a", "c", "f", "d", "e", "g")
+x = sorted(a)
+print(x)
+
+# Sort descending:
+
+a = ("h", "b", "a", "c", "f", "d", "e", "g")
+x = sorted(a, reverse=True)
+print(x)
