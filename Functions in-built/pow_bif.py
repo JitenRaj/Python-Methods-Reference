62c55d7e961f1f85adb2738a51d19fd8d08ea547
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-16 23:37:41 +0530

    pow() function

---
+++ Functions in-built/pow_bif.py
@@ -0,0 +1,11 @@
+# Return the value of 4 to the power of 3 (same as 4 * 4 * 4):
+
+x = pow(4, 3)
+
+print(x)
+
+# Return the value of 4 to the power of 3, modulus 5 (same as (4 * 4 * 4) % 5):
+
+y = pow(4, 3, 5)
+
+print(y)
