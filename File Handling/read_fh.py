66b419347b025f0dd960f0adf4df0fefb0a4b590
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-17 23:32:53 +0530

    read() funtion

--- File Handling/read_fh.py
+++ File Handling/read_fh.py
@@ -1,3 +1,17 @@
+""" To open the file, use the built-in open() function.
+
+The open() function returns a file object, which has a read() method for reading the content of the file:"""
+
 f = open("example.txt","r")
 
+print(f.read())
+
+# Return the 5 first characters of the file:
+
+f = open("example.txt", "r")
+print(f.read(5))
+
+# Open a file on a different location:
+
+f = open("D:\GitHub\Python-Practice\README.md", "r")
 print(f.read())
