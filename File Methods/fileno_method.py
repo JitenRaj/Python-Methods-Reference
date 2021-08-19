fcc481a893158634a967c887b6116fd6dbef6cf5
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-19 22:53:48 +0530

    fileno() method

---
+++ File Methods/fileno_method.py
@@ -0,0 +1,9 @@
+# Return the file descriptor of the stream:
+
+f = open("demofile.txt", "r")
+print(f.fileno())
+
+
+""" The fileno() method returns the file descriptor of the stream, as a number.
+
+An error will occur if the operator system does not use a file descriptor. """
