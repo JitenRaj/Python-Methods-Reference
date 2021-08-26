5b509a9af70fdf339c7f239a1f3b3c101c3bcdd1
ji10raj <ji10raj@gmail.com>
2021-08-26 23:28:01 +0530

    items() method

---
+++ Dictionary Methods/items_dm.py
@@ -0,0 +1,25 @@
+# Return the dictionary's key-value pairs:
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.items()
+
+print(x)
+
+# When an item in the dictionary changes value, the view object also gets updated:
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.items()
+
+car["year"] = 2018
+
+print(x)
