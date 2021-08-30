7c577d0148d5ce71c32d3fce6e862e85d87e00a6
ji10raj <ji10raj@gmail.com>
2021-08-30 23:51:01 +0530

    add() set method

---
+++ Set Methods/add_sm.py
@@ -0,0 +1,15 @@
+# Add an element to the fruits set:
+
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("orange")
+
+print(fruits)
+
+# Try to add an element that already exists:
+
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("apple")
+
+print(fruits)
