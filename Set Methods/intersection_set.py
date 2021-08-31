9a44e9636801ec416cb2a8421dc9e72445fe5e21
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-08-31 21:50:41 +0530

    intersection() method

--- Set Methods/intersection_set.py
+++ Set Methods/intersection_set.py
@@ -6,3 +6,14 @@ y = {"google", "microsoft", "apple"}
 z = x.intersection(y)
 
 print(z)
+
+
+# Compare 3 sets, and return a set with items that is present in all 3 sets:
+
+x = {"a", "b", "c"}
+y = {"c", "d", "e"}
+z = {"f", "g", "c"}
+
+result = x.intersection(y, z)
+
+print(result)
