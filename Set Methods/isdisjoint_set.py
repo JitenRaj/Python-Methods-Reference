03fc218ba791ee002b8c97d77591bbc8b71d1457
ji10raj <ji10raj@gmail.com>
2021-09-01 21:02:03 +0530

    isdisjoint() set method

---
+++ Set Methods/isdisjoint_set.py
@@ -0,0 +1,19 @@
+# Return True if no items in set x is present in set y:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "facebook"}
+
+z = x.isdisjoint(y)
+
+print(z)
+
+# What if no items are present in both sets?
+
+# Return False if one ore more items are present in both sets:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.isdisjoint(y)
+
+print(z)
