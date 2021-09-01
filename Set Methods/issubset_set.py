bb1355b29405a0f5efd4fd7671d65fbea203215b
ji10raj <ji10raj@gmail.com>
2021-09-01 21:05:42 +0530

    issubset() method

---
+++ Set Methods/issubset_set.py
@@ -0,0 +1,19 @@
+# Return True if all items in set x are present in set y:
+
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b", "a"}
+
+z = x.issubset(y)
+
+print(z)
+
+# What if not all items are present in the specified set?
+
+# Return False if not all items in set x are present in set y:
+
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b"}
+
+z = x.issubset(y)
+
+print(z)
